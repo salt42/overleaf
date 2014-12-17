1109a4e9a2f21b3b96589c330912886f0c055770
salt42 <boluuuu@gmail.com>
2014-12-18 00:52:01 +0100

    felix

--- src/main.js
+++ src/main.js
@@ -2,13 +2,27 @@ var req = require.config({
 	context: 'test',
 	packages: [
 		{
-			name: 'game',
-			location: 'libs/mario',
-			main: 'bootGameEngine'
+			name: 'marioEngine',
+			location: 'libs/mario'
 		}
 	]
 });
 
-req(["require", 'game'], function(require, Game){
-	console.log(Game)
+
+req(["require", 'marioEngine'], function(require, Engine){
+	Engine.onReady(function() {
+
+		function Player () {
+			// this.super.constructor(arguments);
+		};
+
+		Player.prototype.test = function(){
+
+		};
+
+		Player.prototype = Engine.getEntityFactory().createEntity('SimpleShape', Player.prototype);
+		console.log( new Player());
+		//var world = Framework.events.
+
+	});
 });
