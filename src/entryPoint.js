fa31f1839f32965dbcf429dcdfdc268b4646b86d
salt42 <boluuuu@gmail.com>
2014-12-18 03:27:18 +0100

    -

---
+++ src/entryPoint.js
@@ -0,0 +1,24 @@
+define(function(require, exports, module)  {
+    var Engine = require('marioEngine'),
+        Player = require('entities/player');
+
+
+
+    exports.init = function() {
+        var core = Engine.getCore();
+            core.setDOM($('#dsadsad'));
+
+        var world = core.createWorld();
+        //fill world
+        var player = new Player();
+        world.addEntity(player);
+        world.addEntity(new BlaEntity());
+        world.addEntity(new BlaEntity());
+        world.addEntity(new BlaEntity());
+        // ...
+        core.setActiveWorld(world);
+        core.registerGameLoopTick(1, function() {});
+        core.start();
+
+    };
+});
--- src/main.js
+++ src/main.js
@@ -1,31 +1,20 @@
-var req = require.config({
-	context: 'test',
-	packages: [
-		{
-			name: 'marioEngine',
-			location: 'libs/mario'
-		}
-	]
-});
-
-
-req(["require", 'marioEngine'], function(require, Engine){
-	Engine.onReady(function() {
-
-		function Player () {
-			//this.prototype.constructor.call(this);
-			this.playerProperty = 42;
-
-			// this.super.constructor(arguments);
-		};
-
-		Player.prototype = Engine.getEntityFactory().createEntity('SimpleShape');
-		Player.prototype.test = function(){
-
-		};
-
-		console.log( new Player());
-		//var world = Framework.events.
-
+define(function(require, exports, module) {
+	var req = requirejs.config({
+		context: 'test',
+		baseUrl: 'src',
+		packages: [
+			{
+				name: 'marioEngine',
+				location: '../libs/mario'
+			}
+		]
 	});
+	req(['marioEngine'], function(Engine){
+		Engine.onReady(function() {
+			req(['entryPoint'], function(EntryPoint){
+				EntryPoint.init();
+			});
+		});
+	});
+
 });
